621e85218548be5c2c1f8081a14bd91a7b923409
Kohsheen Tiku <kohsheen.t@gmail.com>
2019-03-07 13:02:21 +0530

    m

--- front_end/app.js
+++ front_end/app.js
@@ -25,28 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // methodOverride
 app.use(methodOverride("_method"));
 
-// Search Page
-app.get("/", function(req, res, next) {
-  res.render("searchusers");
-});
 
-// Search processing
-app.post("/user/search", function(req, res, next) {
-  let id = req.body.id;
-
-  client.hgetall(id, function(err, obj) {
-    if (!obj) {
-      res.render("searchusers", {
-        error: "User does not exist"
-      });
-    } else {
-      obj.id = id;
-      res.render("details", {
-        user: obj
-      });
-    }
-  });
-});
 
 app.listen(port, function() {
   console.log("Server started on port " + port);
